5f1f1691b49725c2483906821241601b1a9c9a73
PinkDeer <bunman@mail.ru>
2017-07-02 19:12:01 +0300

    Solves exercises 4.1 and 4.2

---
+++ task_4_2.rb
@@ -0,0 +1,7 @@
+params = {}
+
+params[:person1] = {firts: "Andrey", last: "Z"}
+params[:person2] = {firts: "Raya", last: "P"}
+params[:person3] = {firts: "Nikola", last: "T"}
+
+puts params[:person2][:firts]
