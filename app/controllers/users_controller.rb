6afa0545854afb344c20eff09a8d58daca27704f
PinkDeer <bunman@mail.ru>
2017-07-05 20:32:14 +0300

    Add method create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
 
-  def new
+  def show
+    @user = User.find(params[:id])
   end
 
-  def show
-  	@user = User.find(params[:id])
+  def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Обработать успешное сохранение.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 end
