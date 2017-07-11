ccce4fd650959c730b34562616a4a1a4bb4c821b
PinkDeer <bunman@mail.ru>
2017-07-11 14:19:42 +0300

    Edit sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
-end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Осуществить вход пользователя и переадресовать на страницу профиля.
+    else
+      # Создать сообщение об ошибке.
+      render 'new'
+    end
+  end
+  def destroy
+  end
