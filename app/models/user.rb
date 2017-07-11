a01ed9f89bcc59e0fc3995e9bb6975282d28a3fa
PinkDeer <bunman@mail.ru>
2017-07-12 00:05:28 +0300

    Remember user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,6 +9,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
   # Возвращает дайджест для указанной строки.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ?
@@ -14,6 +17,23 @@ class User < ActiveRecord::Base
            BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # Возвращает случайный токен.
+  def User.new_token
+     SecureRandom.urlsafe_base64
+  end
+
+  # Запоминает пользователя в базе данных для использования в постоянных сеансах.
+  def remember
+   self.remember_token = User.new_token
+   update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Возвращает true, если указанный токен соответствует дайджесту.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
 end
 
 # User.create(name: "Michael Hartl", email: "mhartl@example.com", password: "foobar", password_confirmation: "foobar")
