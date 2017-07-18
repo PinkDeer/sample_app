974843c63fed59541e9d70f857b4073dcfc2ae98
PinkDeer <bunman@mail.ru>
2017-07-19 00:03:24 +0300

    Add tests for logout and fix bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,10 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  # Возвращает true, если указанный токен соответствует дайджесту.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
 end
