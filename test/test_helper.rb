6b9243b552c227a9dfe3d08bdb1cdc1c8f5abf85
PinkDeer <bunman@mail.ru>
2017-07-20 15:37:54 +0300

    Finish log in/log out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,3 +18,29 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 end
+
+
+class ActiveSupport::TestCase
+fixtures :all
+  # Возвращает true, если тестовый пользователь вошел.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+  # Выполняет вход тестового пользователя.
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+  private
+    # Возвращает true внутри интеграционного теста.
+    def integration_test?
+      defined?(post_via_redirect)
+    end
+end
